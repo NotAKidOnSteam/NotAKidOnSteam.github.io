ffd2b027da8c9a1783ae1335f3a27c89f791071e
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2019-10-24 23:18:58 -0500

    Added dark mode
    Friend sent screenshot and their browser made it auto darkmode. I liked it so I added a toggle. 100% will be shit on mobile cause yeet

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ setInterval(function(){
 root.querySelector(':first-child').classList.add('is-hidden');
 root.querySelector(':nth-child(2)').classList.remove('is-hidden');
 }, 6000)
+
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
